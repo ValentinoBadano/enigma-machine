d45c0151283cf7347a23570ed6e9bee041506446
ValentinoBadano <valentinobadano@gmail.com>
2024-05-29 20:23:52 -0300

    Adjusted styles

--- enigma.js
+++ enigma.js
@@ -5,21 +5,26 @@ class EnigmaMachine {
         this.plugboard = {};
         this.rotorPositions = [0, 0, 0];
         this.rotorMappings = {
+            // configuración de la Enigma I 
             'I': 'EKMFLGDQVZNTOWYHXUSPAIBRCJ',
             'II': 'AJDKSIRUXBLHWTMCQGZNPYFVOE',
-            'III': 'BDFHJLCPRTXVZNYEIWGAKMUSQO'
+            'III': 'BDFHJLCPRTXVZNYEIWGAKMUSQO',
+            'IV': 'ESOVPZJAYQUIRHXLNFTGKDCMWB',
+            'V': 'VZBRGITYUPSDNHLXAWMJQOFECK'
         };
         this.reflectorMappings = {
-            'B': 'YRUHQSLDPXNGOKMIEBFZCWVJAT'
+            'B': 'YRUHQSLDPXNGOKMIEBFZCWVJAT' ,
+            'C': 'FVPJIAOYEDRZXWGCTKUQSBNMHL'
         };
     }
 
     setPlugboard(settings) {
+        // acepta una cadena de texto con pares de letras separadas por espacios
         this.plugboard = {};
         if (settings) {
             const pairs = settings.split(' ');
             pairs.forEach(pair => {
-                const [a, b] = pair.toUpperCase().split('');
+                const [a, b] = pair.toUpperCase().split(''); 
                 this.plugboard[a] = b;
                 this.plugboard[b] = a;
             });
@@ -72,9 +77,16 @@ class EnigmaMachine {
 }
 
 document.addEventListener('keydown', function(event) {
+    // when a key is pressed, check if it is a letter and if so, encrypt it
     const key = event.key.toUpperCase();
     if (/^[a-zA-Z]$/.test(key)) {
         pressKey(key);
+        // Agregar la clase para la animación
+        document.getElementById("key-" + key).classList.add('pressed');
+        // Eliminar la clase después de 1 segundo
+        setTimeout(function() {
+            document.getElementById(`key-${key}`).classList.remove('pressed');
+        }, 200);
     }
 });
 
@@ -108,16 +120,76 @@ function pressKey(key) {
     document.querySelectorAll('.lamp').forEach(lamp => {
         lamp.classList.remove('on');
     });
+
     const encryptedKey = enigma.encrypt(key);
-    document.getElementById('lamp-' + encryptedKey).classList.add('on');
+    let lamp = document.getElementById('lamp-' + encryptedKey)
+    lamp.classList.add('on');
+
+    setTimeout(function() {
+        lamp.classList.remove('on');
+    }, 2000);
     addToOutputText(encryptedKey);
+    updateRotorCharacters();
+
+}
+
+function updateRotorCharacters() {
+    for (let rotorId = 1; rotorId <= 3; rotorId++) {
+        const prev = document.getElementById('rotor' + rotorId + '-prev');
+        const char = document.getElementById('rotor' + rotorId + '-char');
+        const next = document.getElementById('rotor' + rotorId + '-next');
+        // Set the next character
+        prev.textContent = enigma.rotorMappings[enigma.rotors[rotorId - 1]][(enigma.rotorPositions[rotorId - 1] + 25) % 26];
+        char.textContent = enigma.rotorMappings[enigma.rotors[rotorId - 1]][enigma.rotorPositions[rotorId - 1]];
+        next.textContent = enigma.rotorMappings[enigma.rotors[rotorId - 1]][(enigma.rotorPositions[rotorId - 1] + 1) % 26];
+    }
+}
+
+function changeRotorPosition(rotorId, direction) {
+    enigma.rotorPositions[rotorId - 1] += direction;
+    if (enigma.rotorPositions[rotorId - 1]) {
+    enigma.rotorPositions[rotorId - 1] = (enigma.rotorPositions[rotorId - 1] + 26) % 26;
+    }
     updateRotorCharacters();
 }
 
+let selectedPlugs = [];
+let plugColor = Math.floor(Math.random()*16777215).toString(16);
 
-function updateRotorCharacters() {
-    document.getElementById('rotor1-char').innerText = enigma.rotorMappings["I"][enigma.rotorPositions[0]];
-    document.getElementById('rotor2-char').innerText = enigma.rotorMappings["II"][enigma.rotorPositions[1]];
-    document.getElementById('rotor3-char').innerText = enigma.rotorMappings["III"][enigma.rotorPositions[2]];
+function plug(p) {
+    selectedPlugs.push(p);    
+    selectedPlugs.forEach(plug => {
+        plug.style.backgroundColor = '#' + plugColor;
+    });
+    if (selectedPlugs.length === 2) {
+        plugColor = Math.floor(Math.random()*16777215).toString(16);
+        const [first, second] = selectedPlugs;
+        selectedPlugs = [];
+        enigma.setPlugboard(`${first.innerText}${second.innerText}`);
+        updateRotorCharacters();
+    }
 }
+
+
+function changeRotor(number) {
+    enigma.rotors[number - 1] = document.getElementById('rotor' + number + '-sel').value;
+    updateRotorCharacters();
+}
+
+function changeReflector() {
+    enigma.reflector = document.getElementById('reflector-sel').value;
+    updateRotorCharacters();
+}
+
+function init() {
+    changeRotor(1);
+    changeRotor(2);
+    changeRotor(3);
+    changeReflector();
+    updateRotorCharacters();
+}
+
+init();
+
+
 
